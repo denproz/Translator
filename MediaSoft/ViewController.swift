2f6a8c8435106c472ca795ae47ad9869011cfeea
Denis Prozukin <dprozukin@gmail.com>
2020-09-04 20:11:42 +0300

    Initial Commit

---
+++ MediaSoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaSoft
+//
+//  Created by Denis Prozukin on 04.09.2020.
+//  Copyright © 2020 Denis Prozukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
